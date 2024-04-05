94d76c967f7972d9dfdd756d628e1e0a64a21e48
Victoria <sadovskaya.vicka@yandex.ru>
2024-04-05 14:58:07 +0300

    Update USERS/templates/add_post.html, index.html and page_user.html. Update USERS/user/urls.py and views.py (the correct operation of the login to the page. Add extends and block)

--- USERS/user/urls.py
+++ USERS/user/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('registration/check/', user.registration_check, name='registration_check'),
     path('login/', user.login, name='login'),
     path('login/check/', user.login_check, name='login_check'),
-    path('login/user/<slug:name>/', user.user_page, name='user_page'),
+    path('login/user/<slug:name>/', user.info_user, name='user_page'),
     path('login/user/<slug:name>/posts/', user.add_post, name='add_post'),
 ]
--- USERS/user/views.py
+++ USERS/user/views.py
@@ -15,7 +15,20 @@ users = [
         'password': 'pass_2',
     },
 ]
-posts = []
+posts = [
+    {
+        'name': 'User_1',
+        'title': 'Lorem',
+        'description': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. '
+                       'Iste in neque adipisci distinctio dolor quam!',
+    },
+    {
+        'name': 'User_2',
+        'title': 'Lorem (part 2)',
+        'description': 'Continuation lorem. Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+                       'A hic non nam asperiores nihil adipisci ducimus!',
+    }
+]
 active_user = None
 
 
@@ -50,21 +63,21 @@ def login_check(request):
     global message, active_user
     email = request.GET['email']
     password = request.GET['password']
-    if any([x['email'] == email for x in users] and [x['password'] == password for x in users]):
-        for i in users:
-            if email == i['email']:
+    for i in users:
+        if i['email'] == email:
+            if i['email'] == email and i['password'] == password:
                 active_user = i['name']
-        message = 'Success'
-        url = f'http://127.0.0.1:8000/login/user/{active_user}/'
-    else:
-        url = 'http://127.0.0.1:8000/login/'
-        message = 'Invalid email or password'
-    return render(request, 'login_check.html', context={'message': message, 'url': url})
+                message = 'Success'
+                url = f'http://127.0.0.1:8000/login/user/{active_user}/'
+            else:
+                url = 'http://127.0.0.1:8000/login/'
+                message = 'Invalid email or password'
+            return render(request, 'login_check.html', context={'message': message, 'url': url})
 
 
 def user_page(request, name):
     if any(x['name'] == name for x in users):
-        return render(request, 'page_user.html', context={'name': name, 'posts': posts})
+        return render(request, 'info_user.html', context={'name': name, 'posts': posts})
     else:
         return HttpResponseNotFound('<h1>Error 404. Page not found</h1>')
 
@@ -74,7 +87,18 @@ def add_post(request, name):
     description = request.GET['description']
     if any(x['name'] == name for x in users):
         url = f'http://127.0.0.1:8000/login/user/{name}/'
-        posts.append({'name': name, 'title': title, 'description': description})
-        return render(request, 'add_post.html', context={'url': url})
+        if title == '' or description == '':
+            message = 'Error. Empty fields are not allowed'
+        else:
+            message = 'Success'
+            posts.append({'name': name, 'title': title, 'description': description})
+        return render(request, 'add_post.html', context={'url': url, 'message': message})
+    else:
+        return HttpResponseNotFound('<h1>Error 404. Page not found</h1>')
+
+
+def info_user(request, name):
+    if any(x['name'] == name for x in users):
+        return render(request, 'info_user.html', context={'name': name, 'users': users, 'posts': posts})
     else:
         return HttpResponseNotFound('<h1>Error 404. Page not found</h1>')
