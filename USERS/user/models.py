eebab58b47af31ba5acbd2e22e40fb934069479d
Victoria <sadovskaya.vicka@yandex.ru>
2024-04-07 19:17:56 +0300

    Update USERS/user/models.py

--- USERS/user/models.py
+++ USERS/user/models.py
@@ -3,12 +3,14 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
+    objects = None
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=40)
     password = models.CharField(max_length=20)
 
 
 class Post(models.Model):
+    objects = None
     name = models.CharField(max_length=20)
     title = models.CharField(max_length=40)
     description = models.CharField(max_length=300)
