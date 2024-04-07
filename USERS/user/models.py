15e6a63283503d63e217cfc8df1f5664deff4ba2
Victoria <sadovskaya.vicka@yandex.ru>
2024-04-07 17:28:07 +0300

    Update USERS/templates/index.html, USERS/user/models.py and views.py (incorrect login)

--- USERS/user/models.py
+++ USERS/user/models.py
@@ -6,3 +6,9 @@ class User(models.Model):
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=40)
     password = models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=20)
+    title = models.CharField(max_length=40)
+    description = models.CharField(max_length=300)
--- USERS/user/views.py
+++ USERS/user/views.py
@@ -1,39 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+from .models import *
 
 # Create your views here.
 
-users = [
-    {
-        'name': 'User_1',
-        'email': 'user_1@email.ru',
-        'password': 'pass_1',
-    },
-    {
-        'name': 'User_2',
-        'email': 'user_2@email.ru',
-        'password': 'pass_2',
-    },
-]
-posts = [
-    {
-        'name': 'User_1',
-        'title': 'Lorem',
-        'description': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. '
-                       'Iste in neque adipisci distinctio dolor quam!',
-    },
-    {
-        'name': 'User_2',
-        'title': 'Lorem (part 2)',
-        'description': 'Continuation lorem. Lorem ipsum dolor sit amet consectetur adipisicing elit. '
-                       'A hic non nam asperiores nihil adipisci ducimus!',
-    }
-]
+# posts = [
+#     {
+#         'name': 'User_1',
+#         'title': 'Lorem',
+#         'description': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. '
+#                        'Iste in neque adipisci distinctio dolor quam!',
+#     },
+#     {
+#         'name': 'User_2',
+#         'title': 'Lorem (part 2)',
+#         'description': 'Continuation lorem. Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+#                        'A hic non nam asperiores nihil adipisci ducimus!',
+#     }
+# ]
 active_user = None
+display_users = User.objects.all()
+posts = Post.objects.all()
 
 
 def index(request):
-    return render(request, 'index.html', {'users': users, 'posts': posts})
+    return render(request, 'index.html', {'users': display_users, 'posts': posts})
 
 
 def registration(request):
@@ -49,8 +40,8 @@ def registration_check(request):
     name = request.GET['name']
     email = request.GET['email']
     password = request.GET['password']
-    if all([x['name'] != name for x in users]):
-        users.append({'name': name, 'email': email, 'password': password})
+    if all([x.name != name for x in display_users]):
+        User.objects.create(name=name, email=email, password=password)
         message = 'Success'
         url = 'http://127.0.0.1:8000/'
     else:
@@ -63,20 +54,20 @@ def login_check(request):
     global message, active_user
     email = request.GET['email']
     password = request.GET['password']
-    for i in users:
-        if i['email'] == email:
-            if i['email'] == email and i['password'] == password:
-                active_user = i['name']
-                message = 'Success'
-                url = f'http://127.0.0.1:8000/login/user/{active_user}/'
-            else:
-                url = 'http://127.0.0.1:8000/login/'
-                message = 'Invalid email or password'
-            return render(request, 'login_check.html', context={'message': message, 'url': url})
+    if any(x.email == email for x in display_users):
+        for i in display_users:
+            if i.email == email and i.password == password:
+                active_user = i.name
+        message = 'Success'
+        url = f'http://127.0.0.1:8000/login/user/{active_user}/'
+    else:
+        url = 'http://127.0.0.1:8000/login/'
+        message = 'Invalid email or password'
+    return render(request, 'login_check.html', context={'message': message, 'url': url})
 
 
 def user_page(request, name):
-    if any(x['name'] == name for x in users):
+    if any(x.name == name for x in display_users):
         return render(request, 'info_user.html', context={'name': name, 'posts': posts})
     else:
         return HttpResponseNotFound('<h1>Error 404. Page not found</h1>')
@@ -85,20 +76,20 @@ def user_page(request, name):
 def add_post(request, name):
     title = request.GET['title']
     description = request.GET['description']
-    if any(x['name'] == name for x in users):
+    if any(x.name == name for x in display_users):
         url = f'http://127.0.0.1:8000/login/user/{name}/'
         if title == '' or description == '':
             message = 'Error. Empty fields are not allowed'
         else:
             message = 'Success'
-            posts.append({'name': name, 'title': title, 'description': description})
+            Post.objects.create(name=name, title=title, description=description)
         return render(request, 'add_post.html', context={'url': url, 'message': message})
     else:
         return HttpResponseNotFound('<h1>Error 404. Page not found</h1>')
 
 
 def info_user(request, name):
-    if any(x['name'] == name for x in users):
-        return render(request, 'info_user.html', context={'name': name, 'users': users, 'posts': posts})
+    if any(x.name == name for x in display_users):
+        return render(request, 'info_user.html', context={'name': name, 'users': display_users, 'posts': posts})
     else:
         return HttpResponseNotFound('<h1>Error 404. Page not found</h1>')
