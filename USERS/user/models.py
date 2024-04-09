c42f1e56d01a6ecc3529adb971bba8d8f73ae517
Victoria <sadovskaya.vicka@yandex.ru>
2024-04-09 14:32:18 +0300

    Connecting the database to the admin panel. Update (USERS/user/admin.py and models.py)

--- USERS/user/models.py
+++ USERS/user/models.py
@@ -8,9 +8,15 @@ class User(models.Model):
     email = models.CharField(max_length=40)
     password = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     objects = None
     name = models.CharField(max_length=20)
     title = models.CharField(max_length=40)
     description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
